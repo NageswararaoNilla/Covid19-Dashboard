6c39205fa0481ad8b8ec81ac250abcb838494cc4
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-09-13 04:58:33 +0000

    country stats container added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,28 +2,74 @@ import {Component} from 'react'
 
 import {Link} from 'react-router-dom'
 
+// import {PiQueue} from 'react-icons/pi'
+
+import {IoIosCloseCircle, IoMdMenu} from 'react-icons/io'
+
 import './index.css'
 
 class Header extends Component {
-  render() {
-    return (
-      <nav className="nav-bar-container">
-        <div className="nav-bar">
+  state = {mobileMenuActive: false}
+
+  onClickMenuIcon = () => {
+    this.setState(prevState => ({
+      mobileMenuActive: !prevState.mobileMenuActive,
+    }))
+  }
+
+  showDropDownMenu = () => (
+    <div className="mobile-menu-container">
+      <div className="mobile-menu">
+        <ul className="mobile-menu-list">
           <Link to="/" className="link">
-            <h1 className="logo">
-              COVID19<span className="india-text">INDIA</span>
-            </h1>
+            <li className="mobile-item">Home</li>
           </Link>
-          <ul className="nav-bar-menu">
+          <Link to="/about" className="link">
+            <li className="mobile-item">About</li>
+          </Link>
+        </ul>
+        <button
+          type="button"
+          className="cross-btn"
+          onClick={this.onClickMenuIcon}
+        >
+          <IoIosCloseCircle size={24} color="#ffffff" />
+        </button>
+      </div>
+    </div>
+  )
+
+  render() {
+    const {mobileMenuActive} = this.state
+
+    return (
+      <>
+        <nav className="nav-bar-container">
+          <div className="nav-bar">
             <Link to="/" className="link">
-              <li className="nav-item">Home</li>
+              <h1 className="logo">
+                COVID19<span className="india-text">INDIA</span>
+              </h1>
             </Link>
-            <Link to="/about" className="link">
-              <li className="nav-item">About</li>
-            </Link>
-          </ul>
-        </div>
-      </nav>
+            <ul className="nav-bar-menu">
+              <Link to="/" className="link">
+                <li className="nav-item">Home</li>
+              </Link>
+              <Link to="/about" className="link">
+                <li className="nav-item">About</li>
+              </Link>
+            </ul>
+            <button
+              type="button"
+              className="toggle-btn"
+              onClick={this.onClickMenuIcon}
+            >
+              <IoMdMenu size={28} color="#ffffff" />
+            </button>
+          </div>
+        </nav>
+        {mobileMenuActive && this.showDropDownMenu()}
+      </>
     )
   }
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,139 @@
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import {MdOutlineSearch} from 'react-icons/md'
 
 import Header from '../Header'
 
 import './index.css'
 
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
 class Home extends Component {
+  state = {apiStatus: apiStatusConstants.initial}
+
+  componentDidMount() {
+    this.getAllData()
+  }
+
+  getAllData = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    const apiUrl = 'https://apis.ccbp.in/covid19-state-wise-data'
+    const options = {method: 'GET'}
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const data = await response.json()
+      console.log(data)
+      this.setState({
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  renderNationalData = () => {
+    const {apiStatus} = this.state
+    console.log(apiStatus)
+    return (
+      <>
+        <div className="stats-block" data-testid="countryWideConfirmedCases">
+          <p className="stats-title red">Confirmed</p>
+          <img
+            className="stats-icon"
+            src="https://res.cloudinary.com/dducblq2m/image/upload/v1694250424/Covid19%20Dashboard/check-mark_1_tyldxf.svg"
+            alt="country wide confirmed cases pic"
+          />
+          <p className="stats-count red">342585612</p>
+        </div>
+        <div className="stats-block" data-testid="countryWideActiveCases">
+          <p className="stats-title blue">Active</p>
+          <img
+            className="stats-icon"
+            src="https://res.cloudinary.com/dducblq2m/image/upload/v1694250424/Covid19%20Dashboard/protection_1_wbesfb.svg"
+            alt="country wide confirmed cases pic"
+          />
+          <p className="stats-count blue">342585612</p>
+        </div>
+        <div className="stats-block" data-testid="countryWideRecoveredCases">
+          <p className="stats-title green">Recovered</p>
+          <img
+            className="stats-icon"
+            src="https://res.cloudinary.com/dducblq2m/image/upload/v1694250425/Covid19%20Dashboard/recovered_1_qpdppi.svg"
+            alt="country wide confirmed cases pic"
+          />
+          <p className="stats-count green">342585612</p>
+        </div>
+        <div className="stats-block" data-testid="countryWideDeceasedCases">
+          <p className="stats-title gray">Deceased</p>
+          <img
+            className="stats-icon"
+            src="https://res.cloudinary.com/dducblq2m/image/upload/v1694250424/Covid19%20Dashboard/breathing_1_q9lzyd.svg"
+            alt="country wide confirmed cases pic"
+          />
+          <p className="stats-count gray">342585612</p>
+        </div>
+      </>
+    )
+  }
+
+  renderSuccessView = () => {
+    const {apiStatus} = this.state
+    console.log(apiStatus)
+    return (
+      <>
+        <div className="country-stats">{this.renderNationalData()}</div>
+      </>
+    )
+  }
+
+  renderFailureView = () => (
+    <div>
+      <p>Fetched data failure</p>
+    </div>
+  )
+
+  renderLoadingView = () => (
+    <div className="loader-dots" data-testid="homeRouteLoader">
+      <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+    </div>
+  )
+
+  renderAllHomeData = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      case apiStatusConstants.success:
+        return this.renderSuccessView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      default:
+        return null
+    }
+  }
+
   render() {
     return (
       <div className="app-container">
         <Header />
+        <div className="responsive-container">
+          <div className="search-container">
+            <MdOutlineSearch size={20} color="#94A3B8" />
+            <input
+              type="search"
+              placeholder="Enter the State"
+              className="search-input"
+            />
+          </div>
+          {this.renderAllHomeData()}
+        </div>
       </div>
     )
   }
